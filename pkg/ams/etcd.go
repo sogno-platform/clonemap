ecc2c1b6f2228d39b7aa6d88b62589b9694bfd95
Stefan Daehling <SDaehling@eonerc.rwth-aachen.de>
2020-04-09 14:16:51 +0200

    test

--- pkg/ams/etcd.go
+++ pkg/ams/etcd.go
@@ -65,6 +65,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -428,6 +429,7 @@ func (stor *etcdStorage) initMASImGroups(masID int) (err error) {
 		for j := range resp.Kvs {
 			temp := strings.Split(string(resp.Kvs[j].Key), "/")
 			if len(temp) != 8 {
+				fmt.Println(temp)
 				continue
 			}
 			var agencyID int
@@ -437,6 +439,7 @@ func (stor *etcdStorage) initMASImGroups(masID int) (err error) {
 				return
 			}
 			if agencyID >= stor.mas[masID].ImageGroups.Inst[i].Agencies.Counter {
+				fmt.Println(agencyID)
 				continue
 			}
 			err = json.Unmarshal(resp.Kvs[j].Value,
