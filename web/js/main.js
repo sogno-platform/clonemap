8de28a696743e9701c091afb182e24dd7be02866
Stefan Daehling <SDaehling@eonerc.rwth-aachen.de>
2020-07-01 16:47:54 +0200

    fetch list of mas

--- web/js/main.js
+++ web/js/main.js
@@ -4,14 +4,15 @@ $(document).ready(function(){
 
     $("#sideplatform").click(sideplatform);
 
-    $(".masbutton").click(sidemas);
+    updateSidebar();
 
 });
 
 function sideoverview(){
     $(".modules").hide();
     $("#headertitle").text("Overview");
-    fetch('/api/ams/mas').then(response => response.json()).then(data => console.log(data));
+    updateSidebar();
+    // fetch('/api/ams/mas').then(response => response.json()).then(data => console.log(data));
 }
 
 function sideplatform(){
@@ -22,4 +23,18 @@ function sideplatform(){
 function sidemas(){
     $(".modules").show();
     $("#headertitle").text(this.id);
+}
+
+function updateSidebar(){
+    fetch('/api/ams/mas')
+    .then(response => response.json())
+    // .then(data => JSON.parse(data))
+    .then(mas => {
+        $("#maslist").empty()
+        for (let i of mas) {
+            console.log(i);
+            $("#maslist").append("<li><a href=\"#\" class=\"masbutton\" id=\"sidemas"+i.id.toString()+"\">MAS"+i.id.toString()+"</a></li>")
+        }
+        $(".masbutton").click(sidemas);
+    })
 }
