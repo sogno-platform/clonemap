65f5b03ab66ca62d49d2d40fb2962f650ffcb884
Nydia745 <qianwen.yuan12@gmail.com>
2021-03-06 16:44:44 +0100

    initial-web

--- web/js/main.js
+++ web/js/main.js
@@ -2,26 +2,33 @@ import * as request from "./request.js";
 
 // initial website present
 $(document).ready(function(){
-    $("#sideoverview").click(sideoverview);
-    updateSidebar();
-    sideoverview();
+    $("#navoverview").click(navoverview);
+    $("#overview").click(navoverview);
+    $("#inputMAS").change(inputMAS);
+    $("#uploadMAS").click(uploadMAS);
+    navoverview();
 });
 
 // callback for Overview button
-function sideoverview(){
-    $(".modules").hide();
-    $("#headertitle").text("Overview");
+function navoverview(){
+    $(".modules").css("visibility","hidden");
+    $(".nav-link").removeClass("active");
+    $("#overview").addClass("active");
+    //$("#headertitle").text("Overview");
+    request.get('/api/overview',contentOverview);
     updateSidebar();
-    request.get('/api/overview',contentOverview)
     // fetch('/api/ams/mas').then(response => response.json()).then(data => console.log(data));
 }
 
 // callback for mas buttons
 function sidemas(){
-    $(".modules").show();
-    var masID = this.id.split("sidemas")
-    $("#headertitle").text("MAS"+masID[1]);
-    contentAMS(parseInt(masID[1]))
+    $(".modules").css("visibility","visible");
+    $("#overview").removeClass("active");
+    $(".masbutton").removeClass("active");
+    $('#'+this.id).addClass("active");
+    var masID = this.id.split("sidemas");
+    
+    contentAMS(parseInt(masID[1]));
 }
 
 // show sidebar and register callbacks for mas buttons
@@ -31,40 +38,65 @@ function updateSidebar(){
     // .then(data => JSON.parse(data))
     .then(mas => {
         $("#maslist").empty()
+        if (mas === null) {
+            return;
+        }
         for (let i of mas) {
-            $("#maslist").append("<li><a href=\"#\" class=\"masbutton\" id=\"sidemas"+i.id.toString()+"\">MAS"+i.id.toString()+"</a></li>")
+            $("#maslist").append("<li class='nav-item'><a href=\"#\" class=\"nav-link masbutton\" id=\"sidemas"+i.id.toString()+"\">MAS"+i.id.toString()+"</a></li>")
         }
         $(".masbutton").click(sidemas);
     })
 }
 
+
+
+
 // show content field for overview view
 function contentOverview(mass) {
-    $(".contenttitle").replaceWith("<h2 class=\"contenttitle\">Overview</h2>");
     clearContent();
-    $(".content").append("<div class=\"contentbox\" id=\"startbox\"></div>")
-    $("#startbox").append("<table id=\"startmas\"></table>");
-    $("#startmas").append("<tr><th><h3>Start new MAS:</h3></th></tr>")
-    $("#startbox").append("<hr>");
-    $("#startbox").append("<tr><th><input type=\"file\" id=\"inputMAS\" value=\"Import\" accept=\".json\"/></th><th><button id=\"uploadMAS\">Upload</button></th></tr>")
-    $("#inputMAS").change(inputMAS);
-    $("#uploadMAS").click(uploadMAS);
-    $("#startbox").append("<tr><th><textarea id=\"newMAS\"></textarea></th></tr>")
-    $(".content").append("<div class=\"contentbox\" id=\"massbox\"></div>")
-    $("#massbox").append("<table id=\"mass\"></table>");
-    $("#mass").append("<tr><th><h3>MASs:</h3></th><th>"+mass.length.toString()+"</th></tr>");
-    $("#massbox").append("<hr>");
-    for (let i of mass) {
-        let masID = "MAS"+i.id.toString()
-        $("#massbox").append("<table id=\""+masID+"\"></table>");
-        $("#"+masID).append("<tr><th>"+masID+"</th></tr>");
-        $("#"+masID).append("<tr><th></th><th>Name:</th><th>"+i.config.name+"</th></tr>");
-        $("#"+masID).append("<tr><th></th><th>Agents per agency:</th><th>"+i.config.agentsperagency.toString()+"</th></tr>");
-        $("#"+masID).append("<tr><th></th><th>DF:</th><th>"+i.config.df.active.toString()+"</th></tr>");
-        $("#"+masID).append("<tr><th></th><th>Logging:</th><th>"+i.config.logger.active.toString()+"</th></tr>");
-        $("#"+masID).append("<tr><th></th><th>MQTT:</th><th>"+i.config.mqtt.active.toString()+"</th></tr>");
-        $("#"+masID).append("<tr><th></th><th>Agents:</th><th>"+i.numagents.toString()+"</th></tr>");
+
+    if (mass === null) {
+        fetch("../snippets/empty-overview.html").then(response => {
+            return response.text();
+        }).then(emptyContent => {
+            insertHtml("#contentContainer", emptyContent);
+        }).catch(err => {
+            console.log(err);
+        })
+        
+        return;
     }
+
+    $("#contentContainer").css("background-color", "rgb(240,240,240)");
+    let startHtml = "<div class='contenttitle'><h2 class='lefttitle'>Overview</h2> \
+    <button type='button' class='btn my-btn '\
+    data-toggle='modal' data-target='#agencyModal'>\
+    + New Agency</button></div> \
+    <div class='container' id='tables'></div> ";
+    insertHtml("#contentContainer", startHtml);
+    let finalhtml = "<div class='row'>";
+    fetch("../snippets/MAS-card.html").then(response => {
+        return response.text();
+    }).then(table => {
+        let cnt = 0;
+        for (let i of mass) {
+            cnt++;
+            let html = table;
+            html = insertProperty(html,"count", cnt.toString());
+            html = insertProperty(html,"name", i.config.name);
+            html = insertProperty(html,"agent-per-agency", i.config.agentsperagency.toString());
+            html = insertProperty(html,"DF", i.config.df.active.toString());
+            html = insertProperty(html,"Logging", i.config.logger.active.toString());
+            html = insertProperty(html,"MQTT", i.config.mqtt.active.toString());
+            html = insertProperty(html,"Agents", i.numagents.toString());
+            finalhtml += html;
+        }
+        finalhtml += "</div>";
+        insertHtml("#tables", finalhtml);
+    }).catch(error => {
+        console.log(error);
+    });
+
 }
 
 // callback for inputMAS input button
@@ -87,8 +119,8 @@ function inputMAS() {
 // callback for uploadMAS button
 function uploadMAS() {
     let masconfig = $("#newMAS").val();
-    request.post("/api/ams/mas", masconfig)
-    sideoverview()
+    request.post("/api/ams/mas", masconfig);
+    navoverview();
 }
 
 // request info about mas and call function to show content
@@ -98,42 +130,61 @@ function contentAMS(masID){
 
 // show content field for ams and specified mas
 function showAMSContent(masInfo) {
-    $(".contenttitle").replaceWith("<h2 class=\"contenttitle\">MAS"+masInfo.id.toString()+"</h2>");
-    contentMasInfo(masInfo);
-    console.log(masInfo);
+    clearContent();
+    $(".modules").css("visibility","visible");
+    fetch("../snippets/MAS.html").then(response => {
+        return response.text();
+    }).then( html => {
+        html = insertProperty(html,"name", masInfo.config.name);
+        html = insertProperty(html,"agent-per-agency", masInfo.config.agentsperagency.toString());
+        html = insertProperty(html,"DF", masInfo.config.df.active.toString());
+        html = insertProperty(html,"Logging", masInfo.config.logger.active.toString());
+        html = insertProperty(html,"MQTT", masInfo.config.mqtt.active.toString());
+        insertHtml("#contentContainer", html);
+        let containersHtml = "<div class='row'>";
+        for (let i of masInfo.imagegroups.instances) {
+            containersHtml += "<div class='col-md-4 my-row'> <div class='card'>";
+            containersHtml += ("<h5 class='card-header'>"+i.id.toString()+"</h5><div class='card-body'>");
+            containersHtml += "<table class='table my-row'>";
+            containersHtml += ("<tr><th>Image</th><th>"+i.config.image+"</th></tr>");
+            containersHtml += ("<tr><th>Agencies:</th><th>"+i.agencies.counter.toString()+"</th></tr>");
+            containersHtml += "</table> </div></div></div>"
+        }
+        containersHtml += "</div>";
+        insertHtml("#containers", containersHtml);
+
+        let agentsHtml = "<div class='row'>";
+        for (let i of masInfo.agents.instances) {
+            agentsHtml += "<div class='col-md-4 my-row'> <div class='card'>";
+            containersHtml += ("<h5 class='card-header'>"+i.id.toString()+"</h5><div class='card-body'>");
+            agentsHtml += "<table class='table my-row'>";
+            agentsHtml += ("<tr><th>+Name:</th><th>"+i.spec.name+"</th></tr>");
+            agentsHtml += ("<tr><th>Type:</th><th>"+i.spec.type+"</th></tr>");
+            agentsHtml += ("<tr><th>Address:</th><th>"+i.address.agency+"</th></tr>");
+            agentsHtml += "</table> </div></div></div>";
+        }
+        agentsHtml += "</div>"
+        insertHtml("#agents", agentsHtml);
+    }).catch(error => {
+        console.log(error);
+    });
 }
 
 // clear content field
 function clearContent() {
-    $(".content").empty();
+    $(".contentcontainer").empty();
+    
 }
 
-// show content field for mas info
-function contentMasInfo(masInfo) {
-    clearContent();
-    $(".content").append("<table id=\"masinfoid\"></table>");
-    $("#masinfoid").append("<tr><th>ID:</th><th>"+masInfo.id.toString()+"</th></tr>");
-    $(".content").append("<hr>");
-    $(".content").append("<table id=\"masinfoconfig\"></table>");
-    $("#masinfoconfig").append("<tr><th>Config</th></tr>");
-    $("#masinfoconfig").append("<tr><th></th><th>Name:</th><th>"+masInfo.config.name+"</th></tr>");
-    $("#masinfoconfig").append("<tr><th></th><th>Agents per agency:</th><th>"+masInfo.config.agentsperagency.toString()+"</th></tr>");
-    $("#masinfoconfig").append("<tr><th></th><th>DF:</th><th>"+masInfo.config.df.active.toString()+"</th></tr>");
-    $("#masinfoconfig").append("<tr><th></th><th>Logging:</th><th>"+masInfo.config.logger.active.toString()+"</th></tr>");
-    $("#masinfoconfig").append("<tr><th></th><th>MQTT:</th><th>"+masInfo.config.mqtt.active.toString()+"</th></tr>");
-    $(".content").append("<hr>");
-    $(".content").append("<table id=\"masinfocontainer\"></table>");
-    $("#masinfocontainer").append("<tr><th>Containers</th></tr>");
-    for (let i of masInfo.imagegroups.instances) {
-        $("#masinfocontainer").append("<tr><th></th><th>"+i.id.toString()+":</th><th>"+i.config.image+"</th></tr>");
-        $("#masinfocontainer").append("<tr><th></th><th></th><th>Agencies:</th><th>"+i.agencies.counter.toString()+"</th></tr>");
-    }
-    $(".content").append("<hr>");
-    $(".content").append("<table id=\"masinfoagents\"></table>");
-    $("#masinfoagents").append("<tr><th>Agents</th></tr>");
-    for (let i of masInfo.agents.instances) {
-        $("#masinfoagents").append("<tr><th></th><th>"+i.id.toString()+":</th><th>Name:</th><th>"+i.spec.name+"</th></tr>");
-        $("#masinfoagents").append("<tr><th></th><th></th><th>Type:</th><th>"+i.spec.type+"</th></tr>");
-        $("#masinfoagents").append("<tr><th></th><th></th><th>Address:</th><th>"+i.address.agency+"</th></tr>");
-    }
-}
+// Convenience function for inserting innerHTML for the selector
+let insertHtml = function (selector, html) {
+    let targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+
+let insertProperty = function (string, propName, propValue) {
+    let propToReplace = "{{" + propName + "}}";
+    string = string.replace(new RegExp(propToReplace, "g"), propValue);
+    return string;
+  }
+
