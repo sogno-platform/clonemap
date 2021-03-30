8589296de64deedb84882f5f5d9c43a95fc9e4da
Nydia745 <qianwen.yuan12@gmail.com>
2021-03-30 11:03:39 +0200

    restore the DF

--- web/src/app/pages/df/df.component.ts
+++ web/src/app/pages/df/df.component.ts
@@ -17,22 +17,11 @@ export class DFComponent implements OnInit {
     display: string = "";
     filename: string = "Choose a file...";
     searched_results;
-    curr_state = "list";
-    dashed_connection = [];
-    services = [];
-    comms = [[1, 2], [2, 1], [1, 3], [5, 4], [7, 3], [8,1], [4, 6], [3, 6], [1, 7], [1, 3]]; // fake communications
-    margin: number = 50;
-    interactions = [];
-    rects = [];
-    texts = [];
-    gap_svc: number = 100;
-    gap_comm: number = 50;
     constructor(
         private dfService: DfService,
         private masService: MasService,
         private modalService: NgbModal,
         private route: ActivatedRoute,
-        private renderer2: Renderer2
         ) { }
 
     ngOnInit() {
@@ -64,43 +53,29 @@ export class DFComponent implements OnInit {
                 }
                 
             });
+    }
 
-        for (let i = 1; i <= 8; i++) {
-            let x = i * this.gap_svc;
-            this.dashed_connection.push({x1: x, y1: 150, x2:x, y2:200});
-            this.services.push({x1:x, y1: 200, x2:x, y2: 900,});
-            this.rects.push({x: 60+(i-1)*100, y: 70});
-            this.texts.push({x: 90+(i-1)*100, y: 110})
+        // functions for update the services
+        openLg(content) {
+            this.modalService.open(content, { size: 'lg', centered: true });
         }
-        // create the interaction pairs
-        for (let i = 0; i < this.comms.length; i++) {
-            const top = 250;
-            this.interactions.push({x1:this.comms[i][0] * this.gap_svc, y1: top + i * this.gap_comm, 
-                x2:this.comms[i][1] * this.gap_svc, y2: top + i * this.gap_comm})
+
+        onUpdateContent(content:string) {
+            this.display=content;
         }
         
-    }
-
-    openLg(content) {
-        this.modalService.open(content, { size: 'lg', centered: true });
-    }
-
-    onUpdateContent(content:string) {
-        this.display=content;
-    }
-    
-    handleFileInput(files: FileList) {
-        if (files.length <= 0) {
-            return false;
+        handleFileInput(files: FileList) {
+            if (files.length <= 0) {
+                return false;
+            }
+            this.fileToUpload = files.item(0);
+            let fr = new FileReader();
+            fr.onload = () => {
+                this.display = fr.result.toString();
+                this.filename = this.fileToUpload.name;
+            }
+            fr.readAsText(this.fileToUpload);
         }
-        this.fileToUpload = files.item(0);
-        let fr = new FileReader();
-        fr.onload = () => {
-            this.display = fr.result.toString();
-            this.filename = this.fileToUpload.name;
-        }
-        fr.readAsText(this.fileToUpload);
-    }
 
 
 
@@ -151,15 +126,5 @@ export class DFComponent implements OnInit {
         else {
             this.searched_results = null;
         }
-    }
-
-    onClickList() {
-        this.curr_state = "list";
-        console.log(this.curr_state);
-    }
-
-    onClickUML() {
-        this.curr_state = "UML";
-        console.log(this.curr_state);
     }
 }
