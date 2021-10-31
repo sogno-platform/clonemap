63150020e7cd759dffca3af7b542999295ca3f37
Qianwen <qianwen.yuan12@gmail.com>
2021-11-01 00:02:26 +0100

    fix

--- web/src/app/pages/ams/ams.component.ts
+++ web/src/app/pages/ams/ams.component.ts
@@ -1,31 +1,29 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {  DefaultAMSService } from 'src/app/openapi-services/ams';
 import { ActivatedRoute, Params} from '@angular/router';
-import { MatPaginator } from '@angular/material/paginator';
 import { HttpClient } from '@angular/common/http';
-import { MatSort, SortDirection} from '@angular/material/sort';
-import { merge, Observable, of as observableOf} from 'rxjs';
-import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-ams',
   templateUrl: './ams.component.html',
   styleUrls: ['./ams.component.scss']
 })
-export class AMSComponent implements OnInit, AfterViewInit {
+export class AMSComponent implements OnInit {
 
     MASID: number[] = [];
     MASConfig = [];
     configColumns: string[] = ['name', 'value'];
     containerInfo = [];
     containerColumns: string[] = ['id','image', 'agencies'];
+    agentsInfo = [];
+    agentsColumns: string[] = ['id','name', 'type','agency'];    
     selectedMASID: number = -1;
     selectedMAS: any = null;
     q: number = 1;
     data: any;
 
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-    @ViewChild(MatSort) sort: MatSort;
+
 
     resultsLength = 0;
     isLoadingResults = true;
@@ -39,7 +37,6 @@ export class AMSComponent implements OnInit, AfterViewInit {
 
 
     ngOnInit() {
-
         // get the concrete content of the selected MAS
         this.route.params.subscribe(
             (params: Params) => {
@@ -83,6 +80,20 @@ export class AMSComponent implements OnInit, AfterViewInit {
 
                             })
                         }
+
+                        console.log(this.containerInfo);
+                    
+        
+                    for (const item of selectedMAS.agents.instances) {
+                        this.agentsInfo.push({
+                            id: item.id,
+                            name: item.spec.name,
+                            type: item.spec.type,
+                            agency: item.address.agency
+                        })
+                    }
+     
+
                         
                     });
                 } else {
@@ -91,38 +102,6 @@ export class AMSComponent implements OnInit, AfterViewInit {
         });
     }
 
-    ngAfterViewInit() {
-  
-
-        // If the user changes the sort order, reset back to the first page.
-        this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-    
-        merge(this.sort.sortChange, this.paginator.page)
-          .pipe(
-            startWith({}),
-            switchMap(() => {
-              this.isLoadingResults = true;
-              return this.amsService.getOneMAS(this.selectedMASID)
-                .pipe(catchError(() => observableOf(null)));
-            }),
-            map(data => {
-              // Flip flag to show that loading has finished.
-              this.isLoadingResults = false;
-              this.isRateLimitReached = data === null;
-    
-              if (data === null) {
-                return [];
-              }
-    
-              // Only refresh the result length if there is new data. In case of rate
-              // limit errors, we do not want to reset the paginator to zero, as that
-              // would prevent users from re-triggering requests.
-              this.resultsLength = data.total_count;
-              return data.items;
-              console.log(data);
-            })
-          ).subscribe(data => this.data = data);
-    }
 
 }
 
